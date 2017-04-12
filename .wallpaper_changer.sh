a059e57b9fa16f3bbe268adf33328a0972124aeb
bcclsn <a.buccato@gmail.com>
2017-04-12 16:47:06 +0200

    add wallpaper_changer.sh (gnome)

---
+++ .wallpaper_changer.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+while [[ 1 -eq 1 ]]; do
+
+for i in $(echo /home/bcclsn/Immagini/wallpaper/*.jpg); do
+
+	echo $i
+	gsettings set org.gnome.desktop.background picture-uri file:///${i}
+	sleep 60;
+
+done
+done
