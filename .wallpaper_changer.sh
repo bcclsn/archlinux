7f927833c11bd5eb37792070c660b9164484c152
bcclsn <a.buccato@gmail.com>
2017-05-06 18:20:12 +0200

    update .wallpaper_changer.sh

--- .wallpaper_changer.sh
+++ .wallpaper_changer.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-while [[ 1 -eq 1 ]]; do
-for i in $(echo /home/bcclsn/Immagini/wallpaper/*.jpg); do
-echo $i
-gsettings set org.gnome.desktop.background picture-uri file:///${i}
-sleep 60;
-done
-done
+wallpaperdir='/home/bcclsn/Immagini/wallpaper/'  							#impostare la cartella con gli sfondi desktop
+files=($wallpaperdir/*)
+randompic=`printf "%s\n" "${files[RANDOM % ${#files[@]}]}"`
+gsettings set org.gnome.desktop.background picture-uri $randompic
+#echo $randompic
+sleep 5 												#impostare il delay per il cambio sfondo
+exec $0 												#Avvia una nuova istanza di se stesso impostato sul delay
+exit 0 													#Questo comando non verrà mai eseguito :P
+
+
+#Se vuoi provarlo via terminale, commenta la riga sotto e togli il commento a echo $randompic
