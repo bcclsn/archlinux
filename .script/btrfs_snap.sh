aa85ec9d857ae18c985c2435477b7bf20858fe75
bcclsn <a.buccato@gmail.com>
2017-11-11 10:53:43 +0100

    update snapshot path name

--- .script/btrfs_snap.sh
+++ .script/btrfs_snap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mount -t btrfs -o subvol=/ /dev/sda2 /mnt/btrfs
-btrfs subvolume snapshot / /mnt/btrfs/snap-root/root-$(date +%d.%m.%y--%H:%M)
-#btrfs subvolume snapshot /home /mnt/btrfs/snap-home/home-$(date +%d.%m.%y--%H:%M)
+btrfs subvolume snapshot / /mnt/btrfs/snap-root/root--$(date +%b.%d.%y--%H:%M)
+#btrfs subvolume snapshot /home /mnt/btrfs/snap-home/home--$(date +%b.%d.%y--%H:%M)
 umount /mnt/btrfs
 exit 0
