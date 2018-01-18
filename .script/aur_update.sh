cfc6a078731e9b5d78e3acedd24497e26885594b
bcclsn <a.buccato@gmail.com>
2018-01-18 14:13:44 +0100

    update aur_update.sh to v4.5

--- .script/aur_update.sh
+++ .script/aur_update.sh
@@ -1,11 +1,13 @@
-#!/bin/bash
-
-# aur-update v4.3
+###############################################################################################################
+# aur-update v4.5
 # script di aggiornamento pacchetti installati da AUR, by rebellion
 # preferibilmente da lanciare tramite terminale per visualizzare messaggi di dialogo con lo script
 # a es.: gnome-terminal --hide-menubar --title=\"AUR update\" -e \'sh -c \"sh /home/$USER/aur-update\"\'
 # opzione 'aur-update -p' per inserire il nome pacchetto da aggiornare
 # opzione 'aur-update -p $nomepacchetto' per aggiornare nomepacchetto in automatico
+# gli aggiornamenti sono organizzati in ~/.cache/AUR/$nomepacchetto dove sono presenti PKGBUILD, pkg compilato
+# e lo snapshot scaricato .tar.gz
+###############################################################################################################
 
 controllo_dipendenze () {
    check_dip_aur="ok" ; a="\"Depends\"" ; aa="\",\""
@@ -40,35 +42,13 @@ controllo_dipendenze () {
       done
    else echo "NESSUNA dipendenza per la compilazione richiesta."
    fi
-   echo ; read -s -p "premi invio per continuare..."
-}
-seleziona () {
-   selezione=()
-   row_max=$((${#aur_ok[@]}/4))
-   packages="" ; for ((i=0;i<$row_max;i++)) ; do ii=$(( (i*4)+1 )) ; packages=$packages" "${aur_ok[$ii]} ; done
-   clear ; echo -e "PACCHETTI DA AGGIORNARE:\n\n" ; for ((i=0;i<$row_max;i++)) ; do package=$(echo $packages | awk -v x=$(( $i+1 )) '{print$x}') ; echo "( ) $package" ; done ; echo -e "\n----------------\ncurs.su/curs.giù per scorrere - invio per (de)selezionare - q per uscire\n----------------"
-   tput sc ; tput cuu $(( $row_max+4 )) ; tput cuf 1 ; riga=1
-   while [ "$tasto" != "q" ] ; do
-      read -s -N1
-      ott1="$REPLY"
-      read -s -N2 -t 0.001
-      ott2="$REPLY"
-      read -s -N1 -t 0.001
-      ott3="$REPLY"
-      tasto="$ott1$ott2$ott3"
-      case "$tasto" in
-         $'\x1b\x5b\x41')
-            if [ "$riga" = 1 ] ; then tput cud $(( $row_max-1 )) ; riga=$row_max ; else tput cuu 1 ; riga=$(( $riga-1 )) ; fi ;;
-         $'\x1b\x5b\x42')
-            if [ "$riga" = $row_max ] ; then tput cuu $(( $row_max-1 )) ; riga=1 ; else tput cud 1 ; riga=$(( $riga+1 )) ; fi ;;
-         "
-")
-            if [ "${selezione[$riga]}" = "" ] ; then echo "x" ; selezione[$riga]="$riga"; else echo " " ; selezione[$riga]="" ; fi ; tput cuu 1 ; tput cuf 1 ; if [ "$riga" = $row_max ] ; then tput cuu $(( $row_max-1 )) ; riga=1 ; else tput cud 1 ; riga=$(( $riga+1 )) ; fi ;;
-      esac
-   done
-   tput rc
-   update=""
-   package="" ; for i in "${selezione[@]}" ; do package=$(echo $packages | awk -v x=$i '{print$x}') ; update=$update" "$package ; done
+   pkg="n"
+   if [ -f "$package_dir/$package_name/PKGBUILD" ] ; then
+      echo ; read -sp "Vuoi consultare il PKGBUILD in locale di $package_name prima di aggiornare? [s/n]" pkg
+      if [ "$pkg" = "s" ] ; then clear ; echo -e "${LIGHT_WHITE}$package_dir/$package_name/PKGBUILD\n${NC}" ; cat $package_dir/$package_name/PKGBUILD ; echo ; read -sp "[Premi invio per continuare]" ; fi
+   else
+      echo ; echo "PKGBUILD in $package_dir/$package_name assente." ; read -s -p "[Premi invio per continuare]"
+   fi
 }
 updating () {
       risultato=""
@@ -80,11 +60,11 @@ updating () {
             package_name=$(echo $update | awk -v x=$i '{print$x}')
             echo "Installazione $package_name:"
             package_name_tar=$package_name".tar.gz" ; echo -n "ricerca $package_name_tar..."
-            package_dir_del=$(find /home/$USER -type d -name "$package_name") ; package_dir_del=$(echo $package_dir_del | awk '{print$1}')
-            [[ "$package_dir_del" != "" ]] && package_dir_del_old=$package_dir_del"_old"
-            package_dir=$(find /home/$USER -type d -name "AUR") ; [[ "$package_dir" = "" ]] && package_dir=/home/$USER/AUR && mkdir /home/$USER/.cache/AUR
-            package_path=$(find /home/$USER -type f -name "$package_name_tar") ; package_path=$(echo $package_path | awk '{print$1}')
-            [[ "$package_path" != "" ]] && package_path_old=$package_path".old"
+            package_dir_del="/home/$USER/.cache/$package_name"
+            package_dir="/home/$USER/.cache/AUR" ; [[ -d "$package_dir" ]] || mkdir /home/$USER/.cache/AUR
+            package_path="/home/$USER/.cache/AUR/$package_name_tar" ; package_path_old=$package_path".old"
+            package_dir_name="/home/$USER/.cache/AUR/$package_name"
+            package_dir_name_old=$package_dir_name"_old"
             echo "fatto." ; echo -n "Ricerca versioni..."
             versione_new=$(curl -s "https://aur.archlinux.org/rpc.php?v=5&type=info&arg=$package_name" | grep "Version") ; outofdate=$(echo $versione_new | grep "OutOfDate\":null") ; [[ "$outofdate" = "" ]] && outofdate="(OutOfDate)" || outofdate=""
             versione_new=${versione_new//,/ } ; versione_new=$(echo $versione_new | awk '{print$8}') ; versione_new=${versione_new:11} ; versione_new=${versione_new:0: -1}$outofdate
@@ -93,8 +73,7 @@ updating () {
             echo -e "\nControllo dipendenze del pacchetto $package_name:\n"
             controllo_dipendenze
             if [ "$check_dip_aur" = "ok" ] && dialog --title "pacchetto $package_name" --backtitle "Aggiornamento AUR" --yesno "Verranno installate eventuali dipendenze mancanti presenti in community. Confermi l'aggiornamento?" 7 60 ; then clear
-               [[ -d "$package_dir_del" ]] && mv $package_dir_del $package_dir_del_old
-               [[ "$package_path" != "" ]] && mv $package_path $package_path_old
+               [[ -d "$package_dir_name" ]] && mv $package_dir_name $package_dir_name_old
                echo -e "Aggiornamento "$package_name" in corso, non spengere il pc o la connessione.\nbackup "$package_dir_del"---> fatto\nbackup "$package_name_tar"---> fatto"
                if wget -P $package_dir https://aur.archlinux.org/cgit/aur.git/snapshot/$package_name_tar ; then
                   tar -xvzf $package_dir/$package_name_tar -C $package_dir
@@ -102,20 +81,26 @@ updating () {
                   if makepkg -s ; then
                      echo "Disinstallazione $package_name" ; sudo pacman -R $package_name
                      echo "Installazione $package_name scaricato" ; sudo pacman -U *.pkg.tar.xz
-                     [[ "$package_path_old" != "" ]] && echo -n "Rimozione $package_path_old..." && rm $package_path_old && echo "Fatto."
-                     [[ -d "$package_dir_del_old" ]] && echo -n "Rimozione cartella $package_dir_del_old di backup..." && sudo rm -r $package_dir_del_old && echo "fatto." ; sleep 2
-#pulizia cartella $package_dir/$package_name
+                     [[ -d "$package_dir_name_old" ]] && echo -n "Rimozione $package_dir_name_old di backup..." && rm -r $package_dir_name_old && echo "fatto."
+                     package_pkg=$(find /home/$USER/.cache/AUR/$package_name -type f -name "$package_name*" | grep "pkg.tar.xz") ; mv $package_pkg $package_dir
+                     package_pkgbuild=$(find /home/$USER/.cache/AUR/$package_name -type f -name "$package_name*" | grep "PKGBUILD") ; mv $package_pkg $package_dir
+                     rm -r $package_dir_name ; mkdir $package_dir/$package_name
+                     package_pkg=$(find /home/$USER/.cache/AUR -type f -name "$package_name*" | grep "pkg.tar.xz") ; mv $package_pkg $package_dir_name
+                     package_pkgbuild=$(find /home/$USER/.cache/AUR -type f -name "$package_name*" | grep "PKGBUILD") ; mv $package_pkg $package_dir_name
                      dialog --title "pacchetto $package_name" --backtitle "Aggiornamento AUR" --msgbox "Aggiornamento eseguito!" 7 60 ; clear ; risultato=$risultato" "$package_name"-->installato\n"
                   else
-                     echo "Rimozione cartella $package_dir_del (cartella scompattata $package_name_tar scaricato)..."
+                     echo "Aggiornamento fallito."
+                     echo -n "Rimozione cartella $package_dir_name (cartella scompattata $package_name_tar scaricato) e ripristino backup..."
                      sudo rm -r $package_dir/$package_name
-                     [[ -d "$package_dir_del_old" ]] && mv $package_dir_del_old $package_dir_del
-                     rm $package_dir/$package_name_tar ; [[ "$package_path_old" != "" ]] && mv $package_path_old $package_path
-                     dialog --title "pacchetto $package_name" --backtitle "Aggiornamento AUR" --msgbox "Aggiornamento fallito. Ripristinato il backup del pacchetto." 7 60 ; clear ; risultato=$risultato" "$package_name"-->non_installato_problemi_nella_compilazione(make)\n"
+                     [[ -f "$package_dir"/"$package_name_tar" ]] && rm $package_dir/$package_name_tar
+                     [[ -d "$package_dir_name_old" ]] && mv $package_dir_name_old $package_dir_name
+                     echo "fatto."
+                     read -sp "[Premi invio per continuare]"
+                     risultato=$risultato" "$package_name"-->non_installato_problemi_nella_compilazione(make)\n"
                   fi
                else
-                  [[ -d "$package_dir_del_old" ]] && mv $package_dir_del_old $package_dir_del
-                  [[ "$package_path_old" != "" ]] && mv $package_path_old $package_path
+                  [[ -d "$package_dir_name_old" ]] && mv $package_dir_name_old $package_dir_name
+                  [[ -f "$package_dir"/"$package_name_tar" ]] && rm $package_dir/$package_name_tar
                   dialog --title "pacchetto $package_name" --backtitle "Aggiornamento AUR" --msgbox "Aggiornamento fallito, sembra che AUR non contenga il pacchetto da aggiornare. Ripristinato il backup del pacchetto." 7 60 ; clear
                   risultato=$risultato" "$package_name"-->non_installato_non_presente_in_AUR\n"
                fi
@@ -123,9 +108,6 @@ updating () {
                [[ "$check_dip_aur" = "ok" ]] && risultato=$risultato" "$package_name"-->non_installato_per_scelta_utente\n" || risultato=$risultato" "$package_name"-->non installato per mancanza dipendenze AUR\n"
             fi
          else
-            echo "Connessione assente. Ripristino ultima versione di $package_name..." ; sleep 2 ;
-            [[ -d "$package_dir_del_old" ]] && mv $package_dir_del_old $package_dir_del
-            [[ "$package_path_old" != "" ]] && mv $package_path_old $package_path
             dialog --title "Controllo connessione internet" --backtitle "Aggiornamento AUR" --msgbox "Connessione assente, aggiornamento fallito. $package_name ripristinato all'ultima versione." 7 60 ; clear
             risultato=$risultato" "$package_name"-->non_installato_per_assenza_connessione_internet\n"
          fi
@@ -180,7 +162,6 @@ if ping -q -c 1 -W 1 8.8.8.8 >/dev/null || ping -q -c 1 -W 1 google.com >/dev/nu
       for ((i=0;i<$numero_AUR;i++)) ; do echo -e ${aur[$i]} ; done ; echo -e "\tt=aggiorna tutto | s=seleziona aggiornamenti | altro=esci" ; read -n 1 -s azione
       if [ "$azione" = "s" ] ; then
          update=$(zenity --list --title="PACCHETTI AUR AGGIORNABILI" --text="seleziona per aggiornare" --width=1000 --height=650 --checklist --separator=" " --column="" --column="PACCHETTO" --column="VERSIONE INSTALLATA" --column="VERSIONE DA INSTALLARE" "${aur_ok[@]}" 2>/dev/null) ; numero=$(($(grep -o " " <<<"$update" | wc -l)+1))
-#         seleziona ; numero=$(grep -o " " <<<"$update" | wc -l)
           [[ "${update// /}" != "" ]] && updating
       elif [ "$azione" = "t" ] ; then numero=$((${#aur_ok[@]}/4))
          for ((i=0;i<$numero;i++)) ; do ii=$(( (i*4)+1 )) ; update=$update" "${aur_ok[$ii]} ; done
