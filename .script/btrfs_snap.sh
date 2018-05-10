03da6de41040c1e51359f5c47a9d187ba0d73622
bcclsn <a.buccato@gmail.com>
2018-05-10 14:50:05 +0200

    add compress=lzo in mount option

--- .script/btrfs_snap.sh
+++ .script/btrfs_snap.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-mount -t btrfs -o subvol=/ /dev/sda2 /mnt/btrfs
+mount -t btrfs -o compress=lzo,subvol=/ /dev/sda2 /mnt/btrfs
 btrfs subvolume snapshot / /mnt/btrfs/snap-root/root--$(date +%b.%d.%y--%H:%M)
 #btrfs subvolume snapshot /home /mnt/btrfs/snap-home/home--$(date +%b.%d.%y--%H:%M)
 umount /mnt/btrfs
