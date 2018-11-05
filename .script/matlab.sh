15700de2de2efcc8cb1026437ce7c890662cd833
bcclsn <a.buccato@gmail.com>
2018-11-05 20:16:45 +0100

    fix spacing

--- .script/matlab.sh
+++ .script/matlab.sh
@@ -5,7 +5,7 @@ sudo ip link set dev wlp1s0 down
 sudo ip link set dev wlp1s0 address 66:B7:7E:76:58:9E
 sudo ip link set dev wlp1s0 up
 
-# start matlab and exit#
+# start matlab and exit #
 matlab -desktop
 exit 0
 
