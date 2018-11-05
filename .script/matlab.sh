4ea882cfcfa063ac9a7adabcf9efe89f0572d0e2
bcclsn <a.buccato@gmail.com>
2018-11-05 20:00:03 +0100

    add matlab exec script

---
+++ .script/matlab.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# spoofing mac address #
+sudo ip link set dev wlp1s0 down
+sudo ip link set dev wlp1s0 address 66:B7:7E:76:58:9E
+sudo ip link set dev wlp1s0 up
+
+# start matlab and exit#
+matlab -desktop
+exit 0
+
+################################################
+##                                bcclsn v1.0 ##
+################################################
