8fc5343eba8c0f9d59927472f03fa7e4efa682e3
bcclsn <a.buccato@gmail.com>
2018-11-08 15:12:53 +0100

    rollback

--- .script/matlab.sh
+++ .script/matlab.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # spoofing mac address #
-sudo ip link set dev wlp1s0 down
-sudo ip link set dev wlp1s0 address 66:B7:7E:76:58:9E
-sudo ip link set dev wlp1s0 up
+sudo -S ip link set dev wlp1s0 down
+sudo -S ip link set dev wlp1s0 address 66:B7:7E:76:58:9E
+sudo -S ip link set dev wlp1s0 up
 
 # start matlab and exit #
-matlab -desktop
+#matlab -desktop
 exit 0
 
 ################################################
