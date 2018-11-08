78aabcd99d666633f7e87cd898db334819b54fc4
bcclsn <a.buccato@gmail.com>
2018-11-08 18:21:57 +0100

    rm comment

--- .script/spoofing.sh
+++ .script/spoofing.sh
@@ -5,8 +5,7 @@ sudo -S ip link set dev wlp1s0 down
 sudo -S ip link set dev wlp1s0 address 66:B7:7E:76:58:9E
 sudo -S ip link set dev wlp1s0 up
 
-# start matlab and exit #
-#matlab -desktop
+# exit #
 exit 0
 
 ################################################
