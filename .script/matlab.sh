53ffdb036920fd369ac3d4d14a4eb99c5895cead
bcclsn <a.buccato@gmail.com>
2018-11-08 19:01:25 +0100

    now run matlab in a tmux session that after 20 seconds is killed

--- .script/matlab.sh
+++ .script/matlab.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# get current mac address (random) and define matlab address #
+# get current mac address and define matlab one #
 curr_mac=$(ip link show wlp1s0 | grep "ether" | cut -c 16- | cut -c -17)
 matl_mac="66:B7:7E:76:58:9E"
 
@@ -9,8 +9,9 @@ sudo -S ip link set dev wlp1s0 down
 sudo -S ip link set dev wlp1s0 address $matl_mac
 sudo -S ip link set dev wlp1s0 up
 
-# start matlab #
-matlab -desktop
+# start matlab in a tmux session #
+tmux new-session -d -s matlab 'matlab -desktop'
+sleep 20
 
 # restore previous address and exit #
 sudo -S ip link set dev wlp1s0 down
@@ -19,5 +20,5 @@ sudo -S ip link set dev wlp1s0 up
 exit 0
 
 ################################################
-##                                bcclsn v2.0 ##
+##                                bcclsn v2.1 ##
 ################################################
