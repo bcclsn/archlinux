51b62ba3c5ffe998d2a5dceffc0ca063ed0a5aa4
bcclsn <a.buccato@gmail.com>
2018-11-10 14:02:24 +0100

    final version v2.1.2: remove -S option in sudo and update to stable

--- .script/matlab.sh
+++ .script/matlab.sh
@@ -5,23 +5,23 @@ curr_mac=$(ip link show wlp1s0 | grep "ether" | cut -c 16- | cut -c -17)
 matl_mac="66:B7:7E:76:58:9E"
 
 # spoofing mac address #
-sudo -S ip link set dev wlp1s0 down
-sudo -S ip link set dev wlp1s0 address $matl_mac
-sudo -S ip link set dev wlp1s0 up
+sudo ip link set dev wlp1s0 down
+sudo ip link set dev wlp1s0 address $matl_mac
+sudo ip link set dev wlp1s0 up
 
 # start matlab in a tmux session #
 tmux new-session -d -s matlab 'matlab -desktop'
 sleep 20
 
 # restore previous address #
-sudo -S ip link set dev wlp1s0 down
-sudo -S ip link set dev wlp1s0 address $curr_mac
-sudo -S ip link set dev wlp1s0 up
+sudo ip link set dev wlp1s0 down
+sudo ip link set dev wlp1s0 address $curr_mac
+sudo ip link set dev wlp1s0 up
 
 # clear sudo cache and exit #
 sudo -k
 exit 0
 
 ################################################
-##                              bcclsn v2.1.1 ##
+##                              bcclsn v2.1.2 ##
 ################################################
