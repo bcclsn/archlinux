656c6ab93c5bb5b24f6ce522da75e12e4e7c2fd5
bcclsn <a.buccato@gmail.com>
2019-01-27 13:10:41 +0100

    update script to v1.1: add update of grub.cfg

--- .script/btrfs_snap.sh
+++ .script/btrfs_snap.sh
@@ -7,10 +7,13 @@ mount -t btrfs -o compress=lzo,subvol=/ /dev/sda2 /mnt/btrfs
 btrfs subvolume snapshot / /mnt/btrfs/snap-root/root--$(date +%b.%d.%y--%H:%M)
 #btrfs subvolume snapshot /home /mnt/btrfs/snap-home/home--$(date +%b.%d.%y--%H:%M)
 
-# umount and exit
+# umount
 umount /mnt/btrfs
+
+# update grub.cfg (grub-btrfs) and exit
+grub-mkconfig -o /boot/grub/grub.cfg
 exit 0
 
 ################################################
-##                                bcclsn v1.0 ##
+##                                bcclsn v1.1 ##
 ################################################
