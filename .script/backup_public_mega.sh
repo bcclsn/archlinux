dd5b22f754b2d8fac044d702ec355f1c258b64c5
bcclsn <a.buccato@gmail.com>
2019-02-04 12:01:34 +0100

    add script for mega backup

---
+++ .script/backup_public_mega.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# mega #
+# setting the confidential variables #
+export PASSPHRASE="insert here your passphrase"
+USER="insert here your account id"
+PASS="insert here your master application key"
+HOST="mega.nz"
+NAME="insert here the name of the backup"
+DIR="set the directory that you would to backup"
+MDIR="set the mega directory"
+
+# doing a monthly full backup (1M) #
+duplicity --full-if-older-than 1M --name=$NAME $DIR mega://$USER:$PASS@$HOST/$MDIR
+
+# deleting full backups older than 2 months (2) #
+duplicity remove-all-but-n-full 2 --force mega://$USER:$PASS@$HOST/$MDIR
+
+# to restore a folder from your backup #
+# comment the previuos command and set the directory
+#RESTORE_DIR=
+#duplicity restore mega://$USER:$PASS@$HOST/$MDIR $RESTORE_DIR
+
+# unsetting the confidential variables #
+unset PASSPHRASE
+
+################################################
+##                                bcclsn v1.0 ##
+################################################
