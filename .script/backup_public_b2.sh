bafdfe38f0f1c7ceda5dc227d63a29b9ade94cb0
bcclsn <a.buccato@gmail.com>
2019-02-06 09:31:29 +0100

    add notification

--- .script/backup_public_b2.sh
+++ .script/backup_public_b2.sh
@@ -21,7 +21,10 @@ duplicity remove-all-but-n-full 2 --force b2://$ACCOUNT_ID:$APP_KEY@$BUCKETS
 #duplicity restore b2://$ACCOUNT_ID:$APP_KEY@$BUCKETS $RESTORE_DIR
 
 # notify #
-notify-send duplicity "backup completato" -i /usr/share/icons/Adwaita/scalable/apps/system-file-manager-symbolic.svg -t 60000
+notify-send "duplicity" \
+            "backup completato" \
+            -i /usr/share/icons/Adwaita/scalable/apps/system-file-manager-symbolic.svg \
+            -t 60000
 
 ################################################
 ##                                bcclsn v1.1 ##
--- .script/backup_public_mega.sh
+++ .script/backup_public_mega.sh
@@ -22,7 +22,10 @@ duplicity remove-all-but-n-full 2 --force mega://$USER:$PASS@$HOST/$MDIR
 #duplicity restore mega://$USER:$PASS@$HOST/$MDIR $RESTORE_DIR
 
 # notify #
-notify-send duplicity "backup completato" -i /usr/share/icons/Adwaita/scalable/apps/system-file-manager-symbolic.svg -t 60000
+notify-send "duplicity" \
+            "backup completato" \
+            -i /usr/share/icons/Adwaita/scalable/apps/system-file-manager-symbolic.svg \
+            -t 60000
 
 ################################################
 ##                                bcclsn v1.1 ##
