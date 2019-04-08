1e41eeb86e7ce56d07c9d63deb7a2e8f524fdf23
bcclsn <a.buccato@gmail.com>
2019-04-08 19:40:32 +0200

    Revert "fix log system"
    This reverts commit afa71779a68a35ebb82faee01331c295cde6da7f.

--- .script/backup_public_mega.sh
+++ .script/backup_public_mega.sh
@@ -18,18 +18,18 @@ if (ping -q -c 1 -W 1 8.8.8.8 >/dev/null || ping -q -c 1 -W 1 google.com >/dev/n
 
    # timestamp #
    echo "" >> $LOG
-   date >> $LOG
+   date | tee $LOG
 
    # doing a monthly full backup (1M) #
-   duplicity --full-if-older-than 1M --name=$NAME --log-file $LOG $DIR mega://$USER:$PASS@$HOST/$MDIR
+   duplicity --full-if-older-than 1M --name=$NAME $DIR mega://$USER:$PASS@$HOST/$MDIR | tee $LOG
 
    # deleting full backups older than 2 months (2) #
-   duplicity remove-all-but-n-full 2 --log-file $LOG --force mega://$USER:$PASS@$HOST/$MDIR
+   duplicity remove-all-but-n-full 2 --force mega://$USER:$PASS@$HOST/$MDIR | tee $LOG
 
    # to restore a folder from your backup #
    # comment the previuos command and set the directory
    #RESTORE_DIR=
-   #duplicity restore --log-file $LOG mega://$USER:$PASS@$HOST/$MDIR $RESTORE_DIR
+   #duplicity restore mega://$USER:$PASS@$HOST/$MDIR $RESTORE_DIR | tee $LOG
 
    # unsetting the confidential variables #
    unset PASSPHRASE
