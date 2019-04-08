906fbb7b832e6d1cf33c11f5abbcff7bf1e16a9e
bcclsn <a.buccato@gmail.com>
2019-04-08 18:42:41 +0200

    set log file name as variable

--- .script/backup_public_mega.sh
+++ .script/backup_public_mega.sh
@@ -14,21 +14,22 @@ if (ping -q -c 1 -W 1 8.8.8.8 >/dev/null || ping -q -c 1 -W 1 google.com >/dev/n
    NAME="insert here the name of the backup"
    DIR="set the directory that you would to backup"
    MDIR="set the mega directory"
+   LOG="insert path and name"
 
    # timestamp #
    echo "" >> ~/.log/duplicity.log
    date >> ~/.log/duplicity.log
 
    # doing a monthly full backup (1M) #
-   duplicity --full-if-older-than 1M --name=$NAME $DIR mega://$USER:$PASS@$HOST/$MDIR >> ~/.log/duplicity.log
+   duplicity --full-if-older-than 1M --name=$NAME $DIR mega://$USER:$PASS@$HOST/$MDIR >> $LOG
 
    # deleting full backups older than 2 months (2) #
-   duplicity remove-all-but-n-full 2 --force mega://$USER:$PASS@$HOST/$MDIR >> ~/.log/duplicity.log
+   duplicity remove-all-but-n-full 2 --force mega://$USER:$PASS@$HOST/$MDIR >> $LOG
 
    # to restore a folder from your backup #
    # comment the previuos command and set the directory
    #RESTORE_DIR=
-   #duplicity restore mega://$USER:$PASS@$HOST/$MDIR $RESTORE_DIR >> ~/.log/duplicity.log
+   #duplicity restore mega://$USER:$PASS@$HOST/$MDIR $RESTORE_DIR >> $LOG
 
    # unsetting the confidential variables #
    unset PASSPHRASE
