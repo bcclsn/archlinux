6ae93d72361b6afd24779c1b4f6bf11f466ea49b
bcclsn <a.buccato@gmail.com>
2019-04-08 19:04:58 +0200

    aggiunta possibilità di vedere output comando e contemporanemante salvare il log

--- .script/backup_public_mega.sh
+++ .script/backup_public_mega.sh
@@ -17,19 +17,19 @@ if (ping -q -c 1 -W 1 8.8.8.8 >/dev/null || ping -q -c 1 -W 1 google.com >/dev/n
    LOG="insert path and name"
 
    # timestamp #
-   echo "" >> ~/.log/duplicity.log
-   date >> ~/.log/duplicity.log
+   echo "" >> $LOG
+   date | tee $LOG
 
    # doing a monthly full backup (1M) #
-   duplicity --full-if-older-than 1M --name=$NAME $DIR mega://$USER:$PASS@$HOST/$MDIR >> $LOG
+   duplicity --full-if-older-than 1M --name=$NAME $DIR mega://$USER:$PASS@$HOST/$MDIR | tee $LOG
 
    # deleting full backups older than 2 months (2) #
-   duplicity remove-all-but-n-full 2 --force mega://$USER:$PASS@$HOST/$MDIR >> $LOG
+   duplicity remove-all-but-n-full 2 --force mega://$USER:$PASS@$HOST/$MDIR | tee $LOG
 
    # to restore a folder from your backup #
    # comment the previuos command and set the directory
    #RESTORE_DIR=
-   #duplicity restore mega://$USER:$PASS@$HOST/$MDIR $RESTORE_DIR >> $LOG
+   #duplicity restore mega://$USER:$PASS@$HOST/$MDIR $RESTORE_DIR | tee $LOG
 
    # unsetting the confidential variables #
    unset PASSPHRASE
