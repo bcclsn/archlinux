b3cb80b36bbcde27e6eafcaddb48a0ced2888e88
bcclsn <a.buccato@gmail.com>
2019-06-14 16:43:44 +0200

    aumentato numero snapshot da conservare

--- .script/btrfs_delete.sh
+++ .script/btrfs_delete.sh
@@ -8,9 +8,9 @@ mount -t btrfs -o compress=lzo,subvol=/ /dev/sda2 /mnt/btrfs
 
 # remove the snapshot #
 cd /mnt/btrfs/snap-root
-btrfs subvolume delete "$(ls -1 | head -n -2)"
+btrfs subvolume delete "$(ls -1 | head -n -3)"
 cd /mnt/btrfs/snap-home
-btrfs subvolume delete "$(ls -1 | head -n -2)"
+btrfs subvolume delete "$(ls -1 | head -n -3)"
 cd ~/
 
 # update grup.cfg (grub-btrfs) #
