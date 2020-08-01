b884d883c58b35a807eff11dc6f790aa285913d9
bcclsn <a.buccato@gmail.com>
2020-08-01 20:34:34 +0200

    update readme and add script linking .desktop

--- .dotfiles/README.md
+++ .dotfiles/README.md
@@ -1,5 +1,6 @@
 ## bcclsn - updated to 2020.08.01
 
-- eseguire con permessi di root
+- eseguire install.sh con permessi di root
+- desktop.sh non necessita di permessi di root
 - non spostare e/o rinominare i file e le cartelle dopo aver eseguito il link
 - in caso di spostamento e/o rinomina, editare e rilanciare lo script oppure rieseguire il linking manualmente
---
+++ .dotfiles/desktop.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+USER="bcclsn"
+
+echo "Linking Dotfiles..."
+
+cd /home/$USER/.local/share/applications 
+
+rm wallpaper.desktop freecad.desktop matlab.desktop
+
+ln -s /home/$USER/.desktop/wallpaper.desktop wallpaper.desktop
+ln -s /home/$USER/.desktop/freecad.desktop freecad.desktop
+ln -s /home/$USER/.desktop/matlab.desktop matlab.desktop
+
+sleep 5
+echo "Done"
+
+################################################
+##                                bcclsn v1.0 ##
+################################################
