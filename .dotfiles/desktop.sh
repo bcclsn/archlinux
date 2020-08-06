06581563f5d5efc4a2751bfa5f21537d005317e7
bcclsn <a.buccato@gmail.com>
2020-08-06 10:17:45 +0200

    add chibistudio

--- .dotfiles/desktop.sh
+++ .dotfiles/desktop.sh
@@ -6,11 +6,12 @@ echo "Linking Dotfiles..."
 
 cd /home/$USER/.local/share/applications 
 
-rm wallpaper.desktop freecad.desktop matlab.desktop
+rm wallpaper.desktop freecad.desktop matlab.desktop ChibiStudio.desktop
 
 ln -s /home/$USER/.desktop/wallpaper.desktop wallpaper.desktop
 ln -s /home/$USER/.desktop/freecad.desktop freecad.desktop
 ln -s /home/$USER/.desktop/matlab.desktop matlab.desktop
+ln -s /home/$USER/.desktop/ChibiStudio.desktop ChibiStudio.desktop
 
 sleep 5
 echo "Done"
