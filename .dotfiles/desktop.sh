7450343eaf22cdaf44584e554bd3d3b9bcdfbb58
bcclsn <a.buccato@gmail.com>
2021-04-20 10:56:00 +0200

    add kinetic-scroll option in firefox and add teams.desktop: now using browser

--- .dotfiles/desktop.sh
+++ .dotfiles/desktop.sh
@@ -6,12 +6,14 @@ echo "Linking Dotfiles..."
 
 cd /home/$USER/.local/share/applications 
 
-rm wallpaper.desktop freecad.desktop matlab.desktop ChibiStudio.desktop
+rm wallpaper.desktop freecad.desktop matlab.desktop ChibiStudio.desktop firefox.desktop microsoft-teams.desktop
 
 ln -s /home/$USER/.desktop/wallpaper.desktop wallpaper.desktop
 ln -s /home/$USER/.desktop/freecad.desktop freecad.desktop
 ln -s /home/$USER/.desktop/matlab.desktop matlab.desktop
 ln -s /home/$USER/.desktop/ChibiStudio.desktop ChibiStudio.desktop
+ln -s /home/$USER/.desktop/firefox.desktop firefox.desktop
+ln -s /home/$USER/.desktop/microsoft-teams.desktop microsoft-teams.desktop
 
 sleep 5
 echo "Done"
