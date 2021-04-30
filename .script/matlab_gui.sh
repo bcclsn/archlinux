e9c37000589b26deaac43dfd46632a441d29f37c
bcclsn <a.buccato@gmail.com>
2021-04-30 18:35:49 +0200

    rinominato

--- .script/matlab_gui.sh
+++ .script/matlab_gui.sh
@@ -2,18 +2,19 @@
 
 # get current mac address and define matlab one #
 curr_mac=$(ip link show wlp1s0 | grep "ether" | cut -c 16- | cut -c -17)
-matl_mac="66:B7:7E:76:58:9E"
+matl_mac="C6:8E:6C:46:F3:5A"
+
+# get password
+password=$(zenity --password --title "MAC Spoofing" --timeout=10)
 
 # spoofing mac address #
-if (zenity --password --title "MAC Spoofing" | \
-    (sudo -S ip link set dev wlp1s0 down & \
-     sudo -S ip link set dev wlp1s0 address $matl_mac & \
-     sudo -S ip link set dev wlp1s0 up)\
-   ) ; then
+if (echo $password | (sudo -S ip link set dev wlp1s0 down & \
+                      sudo -S ip link set dev wlp1s0 address $matl_mac & \
+                      sudo -S ip link set dev wlp1s0 up)) ; then
 
     # start matlab in a tmux session #
-    tmux new-session -d -s "matlab" matlab -desktop
-    sleep 20
+    tmux new-session -d -s "matlab" env MATLAB_JAVA=/usr/lib/jvm/java-8-openjdk/jre matlab -desktop
+    sleep 30
 
     # restore previous address and exit #
     sudo -S ip link set dev wlp1s0 down & \
@@ -32,5 +33,5 @@ sudo -k
 exit 0
 
 ################################################
-##                                bcclsn v2.5 ##
+##                                bcclsn v2.2 ##
 ################################################
